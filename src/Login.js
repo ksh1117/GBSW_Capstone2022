2fd316ce09a692ac43a4dfbd9aa7fdfb2e8a6896
ksh1117 <rlatjdgml5916@naver.com>
2022-05-25 15:29:58 +0900

    로그인, 회원가입, 첫번쨰 페이지 등등 수정

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import './App.css';
-import { Link } from "react-router-dom";
+
 const Login = () => {
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
@@ -11,12 +11,13 @@ const Login = () => {
   const onpwHandler = (e) => {
     setPw(e.currentTarget.value);
   }
+  
 
   return(
     <div className = "center">
     <div className = "Login">
       <form>
-      <h2>일단 캡스톤 로그인폼</h2>
+      <h2>Login</h2>
       <div>
         <input type="text" value={id} onChange = {onIdHandler} placeholder = "아이디" />
       </div>
@@ -24,9 +25,9 @@ const Login = () => {
       <input type="password" value={pw} onChange = {onpwHandler} placeholder = "비밀번호" />
       </div>
       <div>
-      <Link to="/signup">
+
         <button>로그인</button>
-      </Link>
+
       </div>
       </form>
       </div>

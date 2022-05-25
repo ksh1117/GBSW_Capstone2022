8dfed58cef54ab178bc330c88cac835b5500580a
hanchaeyoung <gks4167@naver.com>
2022-05-25 18:59:58 +0900

    메인페이지 수정함

--- learnchart/src/Mainpage.js
+++ learnchart/src/Mainpage.js
@@ -1,98 +1,104 @@
 import React from "react";
 import './Mainpage.css';
+import logoImg from './assets/logoImg.png';
+import profileImg from './assets/profileImg.png';
+import capstone from './assets/capstone.jpg'
+import menu from './assets/menu.png';
 
 const Mainpage = () => {
   return (
     <div className="center1">
       <header>
         <div className="lineImg">
-          <div>
-            <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E %3Cpath fill='%23000' fill-rule='nonzero' d='M22.5 18a.5.5 0 1 1 0 1h-21a.5.5 0 1 1 0-1h21zm0-6a.5.5 0 1 1 0 1h-21a.5.5 0 1 1 0-1h21zm0-6a.5.5 0 1 1 0 1h-21a.5.5 0 0 1 0-1h21z'/%3E %3C/svg%3E" />
+          <div className="lineImgSort">
+            <img src={menu} />
           </div>
         </div>
 
         <div className="logo">
-          <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="로고" />
+          <img src={logoImg} alt="로고" />
         </div>
 
         <div className="profile">
-            <img src="https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927" alt="프로필" />
+            <img src={profileImg} alt="프로필" />
         </div>
       </header>
 
       <div className="headerSort"></div>
 
       <div class="postDiv">
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
+        <div className="postSort">
+          <div class="post">
+            <div className="postImg">
+              <img src={capstone} alt="타이틀 이미지" />
             </div>
             <div className="text">
-                가나다라마바사아자차카타파하오리오리오리ㅇㅇㅇㅇㅇ오이리어리ㄴㅇ러ㅣㅏㄴ어ㅣㅏ니어ㅣㅏㄴ어리ㅏ너ㅣㄴㅇ러ㅣsdljflksjdsk;fkddfljsdlkjflkdsjkljlksdjklsljfklsdjklfjsdlkjfkldsjflklklsdjflk
+              C언어 1일차
             </div>
-        </div>
+          </div>
 
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
-            </div>
-            <div className="text">
-                dlals;
-            </div>
-        </div>
+          <div class="post">
+              <div className="postImg">
+                  이미지
+              </div>
+              <div className="text">
+                  제목
+              </div>
+          </div>
 
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
-            </div>
-            <div className="text">
-                dlals;
-            </div>
-        </div>
+          <div class="post">
+              <div className="postImg">
+                  이미지
+              </div>
+              <div className="text">
+                  제목
+              </div>
+          </div>
 
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
-            </div>
-            <div className="text">
-                dlals;
-            </div>
-        </div>
+          <div class="post">
+              <div className="postImg">
+                  이미지
+              </div>
+              <div className="text">
+                  제목
+              </div>
+          </div>
 
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
-            </div>
-            <div className="text">
-                가나다라마바사아자차카타파하오리오리오리ㅇㅇㅇㅇㅇ오이리어리ㅏㄹ;ㅇ니ㅏㅓㅣㅏㄴ어ㅣㅇ니이ㅓㅇ니ㅓ리ㅏㄴ어리ㅏ어ㅣㅏㄴ어ㅣ러이너리아너ㅣㅏ런어ㅣ랑너ㄴㅇ러ㅣㅏㄴ어ㅣㅏ니어ㅣㅏㄴ어리ㅏ너ㅣㄴㅇ러ㅣㄱㄱㄱㄱㄱㄱㄱㄱㄷㄹㄴㅇㄴㅇsdljflksjdsk;fkddfljsdlkjflkdsjkljlksdjklsljfklsdjklfjsdlkjfkldsjflklklsdjflk
-            </div>
-        </div>
+          <div class="post">
+              <div className="postImg">
+                  이미지
+              </div>
+              <div className="text">
+                  제목
+              </div>
+          </div>
 
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
-            </div>
-            <div className="text">
-                dlals;
-            </div>
-        </div>
+          <div class="post">
+              <div className="postImg">
+                  이미지
+              </div>
+              <div className="text">
+                  제목
+              </div>
+          </div>
 
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
-            </div>
-            <div className="text">
-                dlals;
-            </div>
-        </div>
+          <div class="post">
+              <div className="postImg">
+                  이미지
+              </div>
+              <div className="text">
+                  제목
+              </div>
+          </div>
 
-        <div class="post">
-            <div>
-                <img src="http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=330:330&_v=20210325103140" alt="타이틀 이미지" />
-            </div>
-            <div className="text">
-                dlals;
-            </div>
+          <div class="post">
+              <div className="postImg">
+                  이미지
+              </div>
+              <div className="text">
+                  제목
+              </div>
+          </div>
         </div>
       </div>
     </div>
